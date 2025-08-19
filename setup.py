fbb741afbe549bd5ebe97f564b1cca3f8767cae1
Aarav J. <aaravjaisingh@gmail.com>
2025-08-19 18:40:56 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup
 setup(
     name="ProlabsRobotics",
     version="0.1.0",
-    py_modules=["ProlabsRobotics"],  # this is your single .py file
-    install_requires=[
-        "pyperclip",
-        "pynput",
-        "pygetwindow; platform_system=='Windows'"
-    ],
-    python_requires=">=3.8",
+    py_modules=["ProlabsRobotics"],
+    description="ProlabsRobotics AI library using PySide6 and QtWebEngine to interact with ChatGPT via a browser backend.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     author="Aarav Jaisingh",
-    author_email="aaravjaisingh@gmail.com",
-    description="Automation + ChatGPT integration with history, by Prolabs Robotics",
     url="https://github.com/Aarav-111/ProlabsRobotics",
+    python_requires=">=3.9",
+    install_requires=[
+        "PySide6>=6.6",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
